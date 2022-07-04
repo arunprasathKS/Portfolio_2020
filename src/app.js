e4bd31e49bdf377fd2c411c86fa1d440da037d2c
arunprasathKS <108670602+arunprasathKS@users.noreply.github.com>
2022-07-04 18:54:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -387,7 +387,7 @@ Ammo().then((Ammo) => {
         new THREE.MeshPhongMaterial({ color: color }), // side
       ];
 
-      var geometry = new THREE.TextGeometry('RYAN FLOYD', {
+      var geometry = new THREE.TextGeometry('ARUNPRASATH', {
         font: font,
         size: 3,
         height: 0.5,
@@ -431,7 +431,7 @@ Ammo().then((Ammo) => {
         new THREE.MeshPhongMaterial({ color: color }), // side
       ];
 
-      var geometry = new THREE.TextGeometry('SOFTWARE ENGINEER', {
+      var geometry = new THREE.TextGeometry('PROJECT ENGINEER', {
         font: font,
         size: 1.5,
         height: 0.5,
